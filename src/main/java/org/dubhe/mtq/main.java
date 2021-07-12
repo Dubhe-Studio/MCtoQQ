1aa8d631110208e9e3cc63968754f37500b31fa7
Gu_ZT <administration@ztxy666.cn>
2021-07-12 13:55:14 +0800

    Initial commit

---
+++ src/main/java/org/dubhe/mtq/main.java
@@ -0,0 +1,20 @@
+package org.dubhe.mtq;
+
+import net.cjsah.bot.console.plugin.Plugin;
+
+import java.util.List;
+
+public abstract class main extends Plugin {
+    public main() {
+        super("MCtoQQ", "0.0.1-Dev", true, List.of("Gu_ZT"));
+    }
+
+    @Override
+    public void onPluginStart() {
+    }
+
+    @Override
+    public void onPluginStop() {
+
+    }
+}
